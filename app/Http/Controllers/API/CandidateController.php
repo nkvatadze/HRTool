2174342f7433dd4061e0a7119172b4f2997faa95
nika.kvatadze <nika.kvatadze@flatrocktech.com>
2022-02-19 22:27:26 +0400

    Added telescope for application monitoring

--- app/Http/Controllers/API/CandidateController.php
+++ app/Http/Controllers/API/CandidateController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Candidate;
 
 class CandidateController extends Controller
 {
     public function index()
     {
-
+        $candidate = Candidate::latest()->get();
     }
 
     public function store()
